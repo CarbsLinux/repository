e1cc2511d04693165deec22fb448bac9838f57c2
Cem Keylan <cem@ckyln.com>
2020-03-17 12:55:24 +0300

    meta: sync core with KISS

--- core/libressl/files/update-certdata.sh
+++ core/libressl/files/update-certdata.sh
@@ -9,8 +9,6 @@
 
 cd /etc/ssl && {
     wget https://curl.haxx.se/ca/cacert.pem
-    mkdir -p /etc/ssl/certs
-    cp -f cacert.pem certs/ca-certificates.crt
     mv -f cacert.pem cert.pem
     printf '%s\n' "${0##*/}: updated cert.pm"
 }

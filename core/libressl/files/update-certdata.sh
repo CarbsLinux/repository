48aaf35e254974960a0f7f6fe11c51e4e1e542d9
Cem Keylan <cem@ckyln.com>
2020-04-06 22:25:20 +0300

    libressl: add KISS_ROOT env

--- core/libressl/files/update-certdata.sh
+++ core/libressl/files/update-certdata.sh
@@ -2,13 +2,13 @@
 #
 # update-certdata.sh
 
-[ -w /etc/ssl ] || {
+[ -w "$KISS_ROOT/etc/ssl" ] || {
     printf '%s\n' "${0##*/}: root required to update cert." >&2
     exit 1
 }
 
-cd /etc/ssl && {
+cd "$KISS_ROOT/etc/ssl" && {
     wget https://curl.haxx.se/ca/cacert.pem
     mv -f cacert.pem cert.pem
-    printf '%s\n' "${0##*/}: updated cert.pm"
+    printf '%s\n' "${0##*/}: updated cert.pem"
 }

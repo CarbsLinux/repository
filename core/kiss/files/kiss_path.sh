2ac80be37b125c7b56557ea182c95148f26c8fa6
Cem Keylan <cem@ckyln.com>
2020-05-29 12:44:51 +0300

    kiss: fix kiss_path

--- core/kiss/files/kiss_path.sh
+++ core/kiss/files/kiss_path.sh
@@ -1,2 +1,2 @@
 export KISS_PATH=/var/db/kiss/repo/core:/var/db/kiss/repo/extra:/var/db/kiss/repo/xorg
-case "$(uname -m)" in i*86) export KISS_PATH=/var/db/kiss/repo/i686:$KISS_PATH
+case "$(uname -m)" in i*86) export KISS_PATH=/var/db/kiss/repo/i686:$KISS_PATH; esac

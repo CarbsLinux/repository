3f17c8c2a94792638ef1849c97862bd1380cbb0e
Cem Keylan <cem@ckyln.com>
2020-08-09 23:52:11 +0300

    META: Fix all usage of kiss, replace all with cpt

--- extra/libressl/files/update-certdata.sh
+++ extra/libressl/files/update-certdata.sh
@@ -2,12 +2,12 @@
 #
 # update-certdata.sh
 
-[ -w "$KISS_ROOT/etc/ssl" ] || {
-    printf '%s\n' "${0##*/}: root required to update cert." >&2
+[ -w "$CPT_ROOT/etc/ssl" ] || {
+    printf '%s\n' "${0##*/}: root required to update CA certificates." >&2
     exit 1
 }
 
-cd "$KISS_ROOT/etc/ssl" && {
+cd "$CPT_ROOT/etc/ssl" && {
     wget https://curl.haxx.se/ca/cacert.pem
     mv -f cacert.pem cert.pem
     printf '%s\n' "${0##*/}: updated cert.pem"
